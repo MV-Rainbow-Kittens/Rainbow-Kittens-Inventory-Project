2bb620c5fc50ceb1fa9b86c2d0259cf5e905795e
Yogue1997 <yousyogue@gmail.com>
2022-07-18 18:02:37 -0400

    Working on the admin componenet

--- rainbow-front-end/src/components/Amin.jsx
+++ rainbow-front-end/src/components/Amin.jsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react"
+import axios from "axios";
+
+
 
 
 function Admin () {
+
+    const [apiData, setDataApi] = useState([])
+
+
+    const api = "http://localhost:8000/api/products";
+
+    const fetch = () => {
+        axios.get(api)
+            .then((res) => {
+                setDataApi(res.data.products)
+            })
+    }
+
+    useEffect(() => {
+        fetch()
+    }, [])
+
+    console.log(apiData);
+
 
     return (
         <div>
@@ -8,9 +31,16 @@ function Admin () {
                 <button>add</button>
                 <p>❌</p>
             </div>
-            <div className="body">
-                
-            </div>
+            {apiData.map( disp => 
+                <div key={disp.id}>
+                    <label>Title : </label>
+                    <input type="text" value={disp.title} readOnly/>
+                    <label > Price : </label>
+                    <input type="text" value={disp.price}  readOnly/>
+                    <button>update</button>
+                    <button>delete</button>
+                </div>
+            )}
         </div>
     )
 }
