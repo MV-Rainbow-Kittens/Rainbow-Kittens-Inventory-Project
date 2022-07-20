379f246ed9db7b1010f093498d5aca9a46770b59
Yogue1997 <yousyogue@gmail.com>
2022-07-20 16:51:28 -0400

    view compoent done

--- rainbow-front-end/src/components/Update.jsx
+++ rainbow-front-end/src/components/Update.jsx
@@ -1,20 +1,18 @@
-import { useState, useEffect } from 'react'
-import '../css/Update.css'
-import { useNavigate, useParams } from 'react-router-dom'
-import axios from "axios"
-import { toast } from 'react-toastify'
-
+import { useState, useEffect } from "react";
+import "../css/Update.css";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 function Update() {
-
-    //with one of react router method we are able to grab a specific id number 
+    //with one of react router method we are able to grab a specific id number
     //from admin component so that we can be able to update that data
-    const params = useParams()
-    const id = params.id
+    const params = useParams();
+    const id = params.id;
     /* * useParams end here * */
 
     //Navigate will be helping us to be redirect to any pages that we want
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     /* * end of navigate * */
 
     //Our api
@@ -22,18 +20,16 @@ function Update() {
     /* * api end here * */
 
     //States
-    const [first_name, setFName] = useState("")
-    const [last_name, setLName] = useState("")
-    const [email, setEmail] = useState("")
-    const [profile_pic, setImage] = useState("")
-    const [job_title, setJob] = useState("")
-    const [location, setLocation] = useState("")
-    const [avatar, setAvatar] = useState("")
-    const [password, setPassword] = useState("")
-    // const [projects, setProject] = useState("")
+    const [first_name, setFName] = useState("");
+    const [last_name, setLName] = useState("");
+    const [email, setEmail] = useState("");
+    const [profile_pic, setImage] = useState("");
+    const [job_title, setJob] = useState("");
+    const [location, setLocation] = useState("");
+    const [avatar, setAvatar] = useState("");
+    const [password, setPassword] = useState("");
+    const [projects, setProject] = useState("")
 
-
-    
     /* * states end here * */
 
     //This function is only fetching data and passing their value to the states
@@ -52,103 +48,164 @@ function Update() {
         setAvatar(data.singleUser.avatar);
         setPassword(data.singleUser.password);
         setProject(data.singleUser.projects);
-    }
+    };
 
     const updateUser = async (updatedUser) => {
         try {
             const res = await fetch(`http://localhost:8000/api/users/update/${id}`, {
-                method: 'PUT',
+                method: "PUT",
                 headers: {
-                    'Content-Type': 'application/json'
+                    "Content-Type": "application/json",
                 },
-                body: JSON.stringify(updatedUser)
+                body: JSON.stringify(updatedUser),
+                
             });
             const data = await res.json();
         } catch (error) {
             console.log(error.message);
         }
-    }
+        navigate('/admin')
+    };
 
     //we want to use this function in the save button. Once we click we want this function tu run
     const handleSubmit = (e) => {
-
         e.preventDefault();
         const updatedUser = {
-            first_name: fName, last_name: lName, email, password, profile_pic: image, job_title: job, location, avatar, projects: project
-        }
+            first_name,
+            last_name,
+            email,
+            password,
+            profile_pic,
+            job_title,
+            location,
+            avatar,
+        };
         updateUser(updatedUser);
-
-    }
+    };
     /*  * end of handleSubmit() *  */
 
-
     useEffect(() => {
-        getUser()
-    }, [])
+        getUser();
+    }, []);
 
     // console.log(upId);
 
-
-
-
+    const count = 1
 
     return (
-        <div className='bodyUp'>
-            <div className='containerUp'>
-                <h1 className='titleUp'>Update</h1>
+        <div className="bodyUp">
+            <div className="containerUp">
+                <h1 className="titleUp">Update</h1>
                 <form onSubmit={handleSubmit}>
-                    <div className='user-details-update'>
-                        <div className='input-box-update'>
-                            <label className='detailsUp' id='idUp'>Id</label>
+                    <div className="user-details-update">
+                        <div className="input-box-update">
+                            <label className="detailsUp" id="idUp">
+                                Id
+                            </label>
                             <input type="text" readOnly value={id} />
                         </div>
-                        <div className='input-box-update'>
-                            <label className='detailsUp'>First Name</label>
-                            <input type="text" value={first_name} onChange={(e) => { setFName(e.target.value) }} />
+                        <div className="input-box-update">
+                            <label className="detailsUp">First Name</label>
+                            <input
+                                type="text"
+                                value={first_name}
+                                onChange={(e) => {
+                                    setFName(e.target.value);
+                                }}
+                            />
                         </div>
-                        <div className='input-box-update'>
-                            <label className='detailsUp'>Last Name</label>
-                            <input type="text" value={last_name} onChange={(e) => { setLName(e.target.value) }} />
+                        <div className="input-box-update">
+                            <label className="detailsUp">Last Name</label>
+                            <input
+                                type="text"
+                                value={last_name}
+                                onChange={(e) => {
+                                    setLName(e.target.value);
+                                }}
+                            />
                         </div>
-                        <div className='input-box-update'>
-                            <label className='detailsUp'>Email</label>
-                            <input type="text" value={email} onChange={(e) => { setEmail(e.target.value) }} />
+                        <div className="input-box-update">
+                            <label className="detailsUp">Email</label>
+                            <input
+                                type="text"
+                                value={email}
+                                onChange={(e) => {
+                                    setEmail(e.target.value);
+                                }}
+                            />
                         </div>
-                        <div className='input-box-update'>
-                            <label className='detailsUp'>Picture</label>
-                            <input type="text" value={profile_pic} onChange={(e) => { setImage(e.target.value) }} />
+                        <div className="input-box-update">
+                            <label className="detailsUp">Picture</label>
+                            <input
+                                type="text"
+                                value={profile_pic}
+                                onChange={(e) => {
+                                    setImage(e.target.value);
+                                }}
+                            />
                         </div>
-                        <div className='input-box-update'>
-                            <label className='detailsUp'>Job Title</label>
-                            <input type="text" value={job_title} onChange={(e) => { setJob(e.target.value) }} />
+                        <div className="input-box-update">
+                            <label className="detailsUp">Job Title</label>
+                            <input
+                                type="text"
+                                value={job_title}
+                                onChange={(e) => {
+                                    setJob(e.target.value);
+                                }}
+                            />
                         </div>
-                        <div className='input-box-update'>
-                            <label className='detailsUp'>Avatar</label>
-                            <input type="text" value={avatar} onChange={(e) => { setAvatar(e.target.value) }} />
+                        <div className="input-box-update">
+                            <label className="detailsUp">Avatar</label>
+                            <input
+                                type="text"
+                                value={avatar}
+                                onChange={(e) => {
+                                    setAvatar(e.target.value);
+                                }}
+                            />
                         </div>
-                        <div className='input-box-update'>
-                            <label className='detailsUp'>Location</label>
-                            <input type="text" value={location} onChange={(e) => { setLocation(e.target.value) }} />
+                        <div className="input-box-update">
+                            <label className="detailsUp">Location</label>
+                            <input
+                                type="text"
+                                value={location}
+                                onChange={(e) => {
+                                    setLocation(e.target.value);
+                                }}
+                            />
                         </div>
                         {/* <div className='input-box-update'>
                             <label className='detailsUp'>Project</label>
-
                             <input type="text" value={project} onChange={(e) => { setProject(e.target.value) }} disabled/>
-                        </div>
+                        </div> */}
+                        {/* {projects.map(proj => 
+                            <div className='input-box-update'>
+                                <label>Projects :  </label>
+                                <input type="text" value={proj.projects}  />
+                            </div>
+                            )} */}
 
-                        <div className='input-box-update'>
-                            <label className='detailsUp'>Password</label>
-                            <input type="text" value={password} onChange={(e) => { setPassword(e.target.value) }} />
+                        <div className="input-box-update">
+                            <label className="detailsUp">Password</label>
+                            <input
+                                type="text"
+                                value={password}
+                                onChange={(e) => {
+                                    setPassword(e.target.value);
+                                }}
+                            />
                         </div>
                     </div>
-                    <button className='buttonUpSave'>Save</button>
+                    <button className="buttonUpSave">Save</button>
                 </form>
-                <div className='button-upD' >
-                    <button className='buttonUpCanc' onClick={() => navigate('/admin')}>Cancel</button>
+                <div className="button-upD">
+                    <button className="buttonUpCanc" onClick={() => navigate("/admin")}>
+                        Cancel
+                    </button>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Update
+export default Update;
